a9d424e51a6b42cabc5062bec3a466cac54c477e
explysm <explysm@proton.me>
2026-05-22 01:24:55 -0500

    just another commit

--- common/components/src/main/java/com/my/axe/ui/components/RpcField.kt
+++ common/components/src/main/java/com/my/axe/ui/components/RpcField.kt
@@ -14,6 +14,7 @@ package com.my.axe.ui.components
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,6 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -49,11 +51,7 @@ fun RpcField(
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 8.dp)
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable(enabled = readOnly) { onClick() }
-        ) {
+        Box(modifier = Modifier.fillMaxWidth()) {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = value,
@@ -66,6 +64,16 @@ fun RpcField(
                 isError = isError,
                 singleLine = true
             )
+            if (readOnly) {
+                Box(
+                    modifier = Modifier
+                        .matchParentSize()
+                        .clickable(
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        ) { onClick() }
+                )
+            }
         }
         if (isError) {
             Text(
