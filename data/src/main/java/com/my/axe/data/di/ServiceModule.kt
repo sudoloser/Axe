115998d3e76c5a506e9d15272178ff89fd9696f3
explysm <explysm@proton.me>
2026-05-27 19:15:17 -0500

    fix: update DelegatingDiscordWebSocket instantiation in ServiceModule

--- data/src/main/java/com/my/axe/data/di/ServiceModule.kt
+++ data/src/main/java/com/my/axe/data/di/ServiceModule.kt
@@ -36,14 +36,7 @@ object ServiceModule {
     fun providesDiscordWebsocket(
         logger: Logger
     ): DiscordWebSocket {
-        val customUrl = Prefs[Prefs.REMOTE_GATEWAY_URL, "https://axe-server.onrender.com/"]
-        val customSignature = Prefs[Prefs.REMOTE_GATEWAY_SIGNATURE, ""].ifEmpty { BuildConfig.AXE_APP_SIGNATURE }
-
         return DelegatingDiscordWebSocket(
-            token = Prefs[Prefs.TOKEN, ""],
-            userId = Prefs[Prefs.USER_ID, ""],
-            appSignature = customSignature,
-            serverBaseUrl = customUrl,
             logger = logger
         )
     }
