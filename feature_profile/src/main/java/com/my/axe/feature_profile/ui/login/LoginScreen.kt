605e5472ea991fe8398af79b65016f10641e56f2
sudoloser <explysm@proton.me>
2026-06-26 01:05:22 -0500

    aaa

--- feature_profile/src/main/java/com/my/axe/feature_profile/ui/login/LoginScreen.kt
+++ feature_profile/src/main/java/com/my/axe/feature_profile/ui/login/LoginScreen.kt
@@ -16,6 +16,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
