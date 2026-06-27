53831db7bb1d5b20d30e21dae8b9b7bd24a06210
sudoloser <explysm@proton.me>
2026-06-27 15:37:06 -0500

    aaa

--- feature_settings/src/main/java/com/my/axe/feature_settings/rpc_settings/RpcSettings.kt
+++ feature_settings/src/main/java/com/my/axe/feature_settings/rpc_settings/RpcSettings.kt
@@ -42,13 +42,11 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
